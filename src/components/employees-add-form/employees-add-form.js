fe5bc506c5a9b58fdaa74333d7d72a72bac31fc1
Ulia <lyanusia@gmail.com>
2022-10-19 12:12:53 +0200

    add condition for creating new employee

--- src/components/employees-add-form/employees-add-form.js
+++ src/components/employees-add-form/employees-add-form.js
@@ -1,58 +1,73 @@
-import { Component } from 'react';
+import { Component } from "react";
 
-import './employees-add-form.css';
+import "./employees-add-form.css";
 
 class EmployeesAddForm extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            salary: ''
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: "",
+      salary: "",
+    };
+  }
+
+  onValueChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value, // ES6 синтаксис позволяет с помощью [] записать составное свойство в обʼект
+    });
+  };
+  onSubmited = (e) => {
+    e.preventDefault();
+
+    const { name, salary } = this.state;
+    if (name.length < 3) {
+        console.log('name should be more than 3 symbols');
+        return;
+    }
+    
+    if (salary === "") {
+        console.log('invalid salary');
+        return;
     }
 
-    onValueChange = (e) => {
-        this.setState({
-            [e.target.name]: e.target.value // ES6 синтаксис позволяет с помощью [] записать составное свойство в обʼект
-        })
-    }
-    onSubmited = (e) => {
-        e.preventDefault();
+    this.props.onAdd(name, salary);
+    this.setState({
+      name: "",
+      salary: "",
+    });
+  };
 
-        this.props.onAdd(this.state.name, this.state.salary);
-        this.setState({
-            name:'',
-            salary: ''
-        })
-    }
+  render() {
+    const { name, salary } = this.state;
 
-    render() {
-        const { name, salary } = this.state;
+    return (
+      <div className="app-add-form">
+        <h3>Добавьте нового сотрудника</h3>
+        <form className="add-form d-flex" onSubmit={this.onSubmited}>
+          <input
+            type="text"
+            className="form-control new-post-label"
+            placeholder="Как его зовут?"
+            name="name"
+            value={name}
+            onChange={this.onValueChange}
+          />
+          <input
+            type="number"
+            className="form-control new-post-label"
+            placeholder="З/П в $?"
+            name="salary"
+            value={salary} // теперь ето управляемьій елемент
+            onChange={this.onValueChange}
+          />
 
-        return (
-            <div className="app-add-form">
-                <h3>Добавьте нового сотрудника</h3>
-                <form
-                    className="add-form d-flex" onSubmit={this.onSubmited}>
-                    <input type="text"
-                        className="form-control new-post-label"
-                        placeholder="Как его зовут?"
-                        name="name"
-                        value={name}
-                        onChange={this.onValueChange} />
-                    <input type="number"
-                        className="form-control new-post-label"
-                        placeholder="З/П в $?"
-                        name="salary"
-                        value={salary} // теперь ето управляемьій елемент
-                        onChange={this.onValueChange} />
-
-                    <button type="submit"
-                        className="btn btn-outline-light">Добавить</button>
-                </form>
-            </div>
-        )
-    }
+          <button type="submit" className="btn btn-outline-light">
+            Добавить
+          </button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default EmployeesAddForm;
